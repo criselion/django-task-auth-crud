fdf3ffd1aaa2ff22683c51273190ea0986401dd9
Criselion <nexcloid@gmail.com>
2025-11-05 20:33:42 -0300

    first commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -o errexit
+
+# poetry install
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
---
+++ tasks/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from .models import Task
+from .forms import TaskForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm()}) #muestra el formulario de registro
+    else:
+        if request.POST['password1'] == request.POST['password2']: #verifica que las contraseñas coincidan
+            # register user
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1']) # para crear un nuevo usuario
+                user.save() #para guardar el usuario
+                login(request, user)
+                return redirect('tasks') #Redirije a la vista de tasks después del registro
+            except:
+                return render(request, 'signup.html', {'form': UserCreationForm(),
+                                                       "error": "username already exists"},)
+
+        return render(request, 'signup.html', {'form': UserCreationForm(),
+                                                       "error": "Passwords do not match"})
+
+@login_required
+def tasks(request):
+
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True).order_by('-created')
+    return render(request, 'tasks.html', {'tasks': tasks, 'title': 'Tasks Pending'})
+
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks, 'title': 'Completed Tasks'})
+
+@login_required
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': TaskForm()
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+            'form': TaskForm()
+            ,'error': 'Please provide valid data'
+            })
+
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task'})
+
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'signin.html', {'form': AuthenticationForm,'error': 'pasword or username are incorrect'})
+        else:
+            login(request, user=user)
+            return redirect('tasks')
+
