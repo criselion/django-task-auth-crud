6924a1ebcb9f66c1a562fe570e139b6a0f2b0b35
Criselion <nexcloid@gmail.com>
2025-11-07 01:05:20 -0300

    build.sh update

--- build.sh
+++ build.sh
@@ -1,16 +1,4 @@
-set -o errexit
-
-# poetry install
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-
-python manage.py migrate && python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='$ADMIN_USERNAME').exists():
-    User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
-    print('Superusuario creado exitosamente')
-else:
-    print('El superusuario ya existe')
-"
+pip install -r requirements.txt && \
+python manage.py migrate && \
+python manage.py collectstatic --noinput && \
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@mi-portafolio.com', 'PasswordSeguro123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
