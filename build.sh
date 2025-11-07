9644535ad21035a12c7c6c3a99ff92429009892c
Criselion <nexcloid@gmail.com>
2025-11-07 01:00:58 -0300

    build.sh update

--- build.sh
+++ build.sh
@@ -4,4 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+python manage.py migrate && python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$ADMIN_USERNAME').exists():
+    User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
+    print('Superusuario creado exitosamente')
+else:
+    print('El superusuario ya existe')
+"
